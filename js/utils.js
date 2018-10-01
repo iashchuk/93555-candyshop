908eb5087ce10521b967f7379de59dc6df4aa5a8
iashchuk <221box@list.ru>
2018-10-01 17:09:21 +0300

    Добавляет функцию получения правильного окончания слова

--- js/utils.js
+++ js/utils.js
@@ -61,11 +61,24 @@
     return newArray;
   };
 
+  /**
+   * Функция получения правильного окончания слова
+   * @param {number} number
+   * @param {Array.<string>} meanings
+   * @return {string}
+   */
+  var getDeclension = function (number, meanings) {
+    var cases = [2, 0, 1, 1, 1, 2];
+    return meanings[(number % 100 > 4 && number % 100 < 20) ?
+      2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+  };
+
   window.utils = {
     getRandomElement: getRandomElement,
     getRandomInRange: getRandomInRange,
     shuffleElements: shuffleElements,
     getRandomString: getRandomString,
-    getRandomArray: getRandomArray
+    getRandomArray: getRandomArray,
+    getDeclension: getDeclension
   };
 })();
