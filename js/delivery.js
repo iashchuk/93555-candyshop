b85152910abfbf3c7a5fbd148d52bd33eda75048
iashchuk <221box@list.ru>
2018-10-06 16:30:35 +0300

    Добавляет переменные для модуля доставки

---
+++ js/delivery.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function () {
+
+  var deliverMethod = document.querySelector('.deliver__toggle');
+  var deliverBtnStore = document.querySelector('#deliver__store');
+  var deliverBtnCourier = document.querySelector('#deliver__courier');
+  var deliverStore = document.querySelector('.deliver__store');
+  var deliverCourier = document.querySelector('.deliver__courier');
+  var deliverPoint = document.querySelectorAll('.deliver__store-item');
+
+  var photoOptions = {
+    PATH: 'img/map/',
+    EXTENSION: '.jpg'
+  };
+
+
+})();
