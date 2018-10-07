bff755643ce75359fe37b6a1789e4eb8a1025108
iashchuk <221box@list.ru>
2018-10-07 07:27:04 +0300

    Добавляет активацию сообщения об ошибке

--- js/message.js
+++ js/message.js
@@ -9,6 +9,9 @@
 
   var modalConfirm = document.querySelector('.modal--confirm');
   var modalConfirmClose = modalConfirm.querySelector('.modal__close');
+  var modalError = document.querySelector('.modal--error');
+  var modalErrorClose = modalError.querySelector('.modal__close');
+  var modalErrorMessage = modalError.querySelector('.modal__message');
 
 
   var modalConfirmCloseHandler = function () {
@@ -27,7 +30,27 @@
     document.addEventListener('keydown', confirmMessageEscPressHandler);
   };
 
+
+  var modalErrorCloseHandler = function () {
+    modalError.classList.add('modal--hidden');
+  };
+
+  var errorMessageEscPressHandler = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      modalErrorCloseHandler();
+    }
+  };
+
+  var activateErrorMessage = function (textMessage) {
+    modalError.classList.remove('modal--hidden');
+    modalErrorClose.addEventListener('click', modalErrorCloseHandler);
+    modalErrorMessage.textContent = textMessage;
+    document.addEventListener('keydown', errorMessageEscPressHandler);
+  };
+
+
   window.message = {
+    error: activateErrorMessage,
     confirm: activateConfirmMessage,
   };
 
